f627a476a686c0ec950bff590fc952032ffd8fcd
Alex Ustas <alex.ustas@internet.ru>
2022-12-24 11:52:34 +0300

    init data in search, view

---
+++ search.py
@@ -0,0 +1,2 @@
+def search_result(base: dict, data: str) -> str:
+    return base.get(data, 'Не найдено')
---
+++ view.py
@@ -0,0 +1,6 @@
+def view_info(fio: str, phone: str):
+    print(f'{fio}, {phone}')
+
+
+def get_info() -> str:
+    return input('Введите данные: ')
